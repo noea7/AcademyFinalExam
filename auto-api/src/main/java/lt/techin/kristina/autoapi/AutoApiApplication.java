67f17d991ad69552778cbc9089478cc14afa309b
noea7 <114760692+noea7@users.noreply.github.com>
2023-06-27 08:41:20 +0300

    auto-api initial commit

---
+++ auto-api/src/main/java/lt/techin/kristina/autoapi/AutoApiApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.kristina.autoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoApiApplication.class, args);
+	}
+
+}
---
+++ auto-api/src/test/java/lt/techin/kristina/autoapi/AutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.kristina.autoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
