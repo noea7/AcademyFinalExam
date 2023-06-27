94b578c642c22db3abb6c5477216140f99c6d520
noea7 <114760692+noea7@users.noreply.github.com>
2023-06-27 13:29:03 +0300

    atempted rating

--- auto-ui/src/pages/ViewRepairman.js
+++ auto-ui/src/pages/ViewRepairman.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { TextField } from "@mui/material";
 import jwtDecode from "jwt-decode";
 import { apiUrl } from "../App";
 
 function ViewRepairmanPage() {
   const [repairman, setRepairman] = useState({});
   const [rating, setRating] = useState("");
+  const [value, setValue] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
   const params = useParams();
   const token = localStorage.getItem("jwtToken");
   const role = jwtDecode(token).role;
+  const userId = jwtDecode(token).id;
 
   useEffect(() => {
     fetch(`${apiUrl}/repairman/` + params.id, {
@@ -30,6 +35,26 @@ function ViewRepairmanPage() {
       .then((textResponse) => setRating(textResponse));
   }, [params.id]);
 
+  const postRating = () => {
+    fetch(
+      `${apiUrl}/rating/${params.id}/${userId}/${value}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then((result) => {
+      if (result.ok) {
+        setSuccess(true);
+        setFailure(false);
+      } else {
+        setFailure(true);
+        setSuccess(false);
+      }
+    });
+  };
+
   return (
     <div className="mx-3">
       <h2 className="my-5">Informacija apie meistrą</h2>
@@ -43,6 +68,18 @@ function ViewRepairmanPage() {
           Autoservisas: {repairman?.autoservice?.name}{" "}
           {repairman?.autoservice?.address}
         </p>
+      </div>
+      <div>
+        <TextField
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          id="rating-value"
+          label="Rating"
+          className="form-control mb-3"
+          size="small"
+          required
+        />
+        <button className="btn btn-secondary me-2 mb-5" onClick={postRating}>Įvertinti</button>
       </div>
       {role === "admin" ? (
         <div>
